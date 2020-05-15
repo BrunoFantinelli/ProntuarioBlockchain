7f4603f0341949614ff4b5da803263188cbf2c05
Bruno Fantinelli <bruno.fantinelli0@gmail.com>
2020-05-15 17:37:28 -0300

    Alterações contrato inteligente

--- ProntuarioBlockchain.sol
+++ ProntuarioBlockchain.sol
@@ -3,21 +3,38 @@ pragma experimental ABIEncoderV2;
 
 contract ProntuarioDistribuido {
 
+
     struct Prontuario {
         address medico;
         address paciente;
         string procedimento;
     }
 
+
+    mapping(uint => address) public listaMedicos;
+    mapping(uint => address) public listaPacientes;
+    mapping(uint => Prontuario) prontuarios;
+
     uint idProntuario = 0;
-    mapping (uint => Prontuario) prontuarios;
+    uint idMedico = 0;
+    uint idPaciente = 0;
+
+    public function cadastrarNovoMedico(address medico){
+        listaMedicos[idMedico] = medico;
+        idMedico++;
+    }
+    
+    public function cadastrarNovoPaciente(address paciente){
+        listaPacientes[idPaciente] = paciente;
+        idPaciente++;
+    }
 
     function newProntuario(address medico, address paciente, string memory procedimento) public  {
         prontuarios[idProntuario] = Prontuario(medico, paciente, procedimento);
         idProntuario++;
     }
 
-    function getProntuario(uint id) view public returns (Prontuario memory){
+    function getProntuario(uint id) public view returns (Prontuario memory){
         return prontuarios[id];
     }
 }
