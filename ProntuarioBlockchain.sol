18421026e98bfcc0099b6a98e31786d089084440
Bruno Fantinelli <bruno.fantinelli0@gmail.com>
2020-05-24 15:09:50 -0300

    Novo Contrato Inteligente

--- ProntuarioBlockchain.sol
+++ ProntuarioBlockchain.sol
@@ -10,32 +10,75 @@ contract ProntuarioDistribuido {
         string procedimento;
     }
 
+    struct Medico{
+        address endereco;
+        string nome;
+        bool exist;
+    }
 
-    mapping(uint => address) public listaMedicos;
-    mapping(uint => address) public listaPacientes;
+    struct Paciente{
+        address endereco;
+        string nome;
+        bool exist;
+    }
+
+    mapping(address => Medico) public listaMedicos;
+    mapping(address => Paciente) public listaPacientes;
     mapping(uint => Prontuario) prontuarios;
 
     uint idProntuario = 0;
-    uint idMedico = 0;
-    uint idPaciente = 0;
 
-    function cadastrarNovoMedico(address medico) public {
-        listaMedicos[idMedico] = medico;
-        idMedico++;
+    constructor(string memory _nome) public{
+        listaMedicos[msg.sender].endereco = msg.sender;
+        listaMedicos[msg.sender].nome = _nome;
+        listaMedicos[msg.sender].exist = true;
     }
 
-    function cadastrarNovoPaciente(address paciente) public {
-        listaPacientes[idPaciente] = paciente;
-        idPaciente++;
+    function cadastrarNovoMedico(address medico, string memory nome) public {
+        bool medicoExiste = getMedico(msg.sender);
+        if(medicoExiste){
+            listaMedicos[medico].endereco = medico;
+            listaMedicos[medico].nome = nome;
+            listaMedicos[medico].exist = true;
+        }
     }
 
-    function newProntuario(address medico, address paciente, string memory procedimento) public  {
-        prontuarios[idProntuario] = Prontuario(medico, paciente, procedimento);
-        idProntuario++;
+    function cadastrarNovoPaciente(address paciente, string memory nome) public {
+        bool medicoExiste = getMedico(msg.sender);
+        if(medicoExiste){
+            listaPacientes[paciente].endereco = paciente;
+            listaPacientes[paciente].nome = nome;
+            listaPacientes[paciente].exist = true;
+        }
+    }
+    
+    function getPaciente(address paciente)public view returns (bool aux){
+        bool medicoExiste = getMedico(msg.sender);
+        if(medicoExiste){
+            aux = listaPacientes[paciente].exist;
+            return aux;
+        }
+    }
+    
+    function getMedico(address medico)private view returns (bool aux){
+        aux = listaPacientes[medico].exist;
+        return aux;
+    }
+
+    function newProntuario(address paciente, string memory procedimento) public  {
+        bool medicoExiste = getMedico(msg.sender);
+        bool pacienteExiste = getPaciente(paciente);
+        if(medicoExiste && pacienteExiste){
+            prontuarios[idProntuario] = Prontuario(msg.sender, paciente, procedimento);
+            idProntuario++;
+        }
     }
 
     function getProntuario(uint id) public view returns (Prontuario memory){
-        return prontuarios[id];
+        bool medicoExiste = getMedico(msg.sender);
+        if(medicoExiste){
+            return prontuarios[id];    
+        }
     }
 }
 
