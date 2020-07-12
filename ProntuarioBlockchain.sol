032683cc6e47b38a0f499700530f36d1d336b3a3
Bruno Fantinelli <bruno.fantinelli0@gmail.com>
2020-07-12 16:05:29 -0300

    RBAC

--- ProntuarioBlockchain.sol
+++ ProntuarioBlockchain.sol
@@ -2,84 +2,100 @@ pragma solidity >=0.4.11 <0.7.0;
 pragma experimental ABIEncoderV2;
 
 contract ProntuarioDistribuido {
-
-
+   
+   
+    struct Role {
+        string nome;
+        bool exist;
+    }
+    
+    struct Usuarios {
+        string nome;
+        bool exist;
+        Role role;
+    }
+    
+    
     struct Prontuario {
         address medico;
         address paciente;
         string procedimento;
     }
+    
 
-    struct Medico{
-        address endereco;
-        string nome;
-        bool exist;
-    }
 
-    struct Paciente{
-        address endereco;
-        string nome;
-        bool exist;
-    }
-
-    mapping(address => Medico) public listaMedicos;
-    mapping(address => Paciente) public listaPacientes;
+    mapping(uint => Role) Roles;
+    mapping(address => Usuarios) ListaUsuarios;
     mapping(uint => Prontuario) prontuarios;
 
     uint idProntuario = 0;
+    uint idRoles = 1;
+    uint idUsuarios = 1;
 
-    constructor(string memory _nome) public{
-        listaMedicos[msg.sender].endereco = msg.sender;
-        listaMedicos[msg.sender].nome = _nome;
-        listaMedicos[msg.sender].exist = true;
-    }
 
-    function cadastrarNovoMedico(address medico, string memory nome) public {
-        bool medicoExiste = getMedico(msg.sender);
-        if(medicoExiste){
-            listaMedicos[medico].endereco = medico;
-            listaMedicos[medico].nome = nome;
-            listaMedicos[medico].exist = true;
-        }
-    }
-
-    function cadastrarNovoPaciente(address paciente, string memory nome) public {
-        bool medicoExiste = getMedico(msg.sender);
-        if(medicoExiste){
-            listaPacientes[paciente].endereco = paciente;
-            listaPacientes[paciente].nome = nome;
-            listaPacientes[paciente].exist = true;
-        }
+    modifier onlyAdmin(){
+        require(keccak256(bytes(ListaUsuarios[msg.sender].role.nome)) == keccak256(bytes("Admin")));
+        _;
     }
     
-    function getPaciente(address paciente)public view returns (bool aux){
-        bool medicoExiste = getMedico(msg.sender);
-        if(medicoExiste){
-            aux = listaPacientes[paciente].exist;
-            return aux;
-        }
+    modifier onlyMedico(){
+        require(keccak256(bytes(ListaUsuarios[msg.sender].role.nome)) == keccak256(bytes("Medico")));
+        _;
     }
-    
-    function getMedico(address medico)private view returns (bool aux){
-        aux = listaPacientes[medico].exist;
-        return aux;
+   
+    modifier MedicoAndAdmin(){
+        require(keccak256(bytes(ListaUsuarios[msg.sender].role.nome)) == keccak256(bytes("Medico")) || keccak256(bytes(ListaUsuarios[msg.sender].role.nome)) == keccak256(bytes("Admin")) );
+        _;
+    }
+   
+
+   constructor(string memory _nome) public{
+        Roles[0].nome = "Admin";
+        Roles[0].exist = true;
+        ListaUsuarios[msg.sender].nome = _nome;
+        ListaUsuarios[msg.sender].role = Roles[0];
+        ListaUsuarios[msg.sender].exist = true;
+        newRole("Medico");
+        newRole("Paciente");
+        
     }
 
-    function newProntuario(address paciente, string memory procedimento) public  {
-        bool medicoExiste = getMedico(msg.sender);
+    
+    function newRole(string memory nome) public onlyAdmin {
+        Roles[idRoles].nome = nome;
+        Roles[idRoles].exist = true;
+        idRoles++;
+    }
+    
+    function deleteRole(uint id) public onlyAdmin {
+        Roles[id].exist = false;   
+    }
+
+
+    function newUsuario(address _endereco, string memory _nome, uint idRole) public onlyAdmin {
+        ListaUsuarios[_endereco].nome = _nome;
+        ListaUsuarios[_endereco].role = Roles[idRole];
+        ListaUsuarios[_endereco].exist = true;
+        idUsuarios++;
+    }
+ 
+
+    function getPaciente(address paciente)public MedicoAndAdmin view returns (bool aux) {
+        if(ListaUsuarios[paciente].exist && keccak256(bytes(ListaUsuarios[paciente].role.nome)) == keccak256(bytes("Paciente"))){
+            return true;
+        }
+        return false;
+    }
+
+    function newProntuario(address paciente, string memory procedimento) public onlyMedico {
         bool pacienteExiste = getPaciente(paciente);
-        if(medicoExiste && pacienteExiste){
+        if(pacienteExiste){
             prontuarios[idProntuario] = Prontuario(msg.sender, paciente, procedimento);
             idProntuario++;
         }
     }
 
-    function getProntuario(uint id) public view returns (Prontuario memory){
-        bool medicoExiste = getMedico(msg.sender);
-        if(medicoExiste){
-            return prontuarios[id];    
-        }
+    function getProntuario(uint id) public MedicoAndAdmin view returns (Prontuario memory){
+            return prontuarios[id];
     }
 }
-
-
